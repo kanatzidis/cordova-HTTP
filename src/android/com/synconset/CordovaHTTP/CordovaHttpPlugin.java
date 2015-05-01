7f763857e3a8c4497770d0fe7c6e6852953dd0f9
Gregori Kanatzidis <kanatzidisg@gmail.com>
2015-05-01 07:39:58 -0500

    fix

--- src/android/com/synconset/CordovaHTTP/CordovaHttpPlugin.java
+++ src/android/com/synconset/CordovaHTTP/CordovaHttpPlugin.java
@@ -60,7 +60,7 @@ public class CordovaHttpPlugin extends CordovaPlugin {
             cordova.getThreadPool().execute(get);
         } else if (action.equals("post")) {
             String urlString = args.getString(0);
-            JSONObject params = args.getString(1);
+            String params = args.getString(1);
             JSONObject headers = args.getJSONObject(2);
             //HashMap<?, ?> paramsMap = this.getMapFromJSONObject(params);
             HashMap<String, String> headersMap = this.addToMap(this.globalHeaders, headers);
